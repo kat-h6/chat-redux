1a9af91d924b4263bbd0e4318bc57d88e1b1b9ee
Katharine Hao <hao.katharine@gmail.com>
2021-09-02 15:58:15 +0300

    added emojify

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { emojify } from 'react-emojione';
 
 const Message = (props) => {
   const { author, content, created_at } = props.message;
   return (
     <div className="message">
-      <p>{content}</p>
+      <p>{emojify(content)}</p>
       <p><strong>{created_at}</strong><br />
         by <strong>{author}</strong></p>
     </div>
